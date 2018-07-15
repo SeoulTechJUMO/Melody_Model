d7df64969119dd67c48e17b8d5ca792d6b50a195
WonHyun <coy4728@naver.com>
2018-07-15 17:34:26 +0900

    commit -m "test_5"

--- Untitled-1.py
+++ Untitled-1.py
@@ -27,6 +27,7 @@ class note:
     length = 0
     delta_start = 0
     now_delta = 0
+    last_len = 0
     note_string = ''
     def __init__(self, note, ontime):
         self.note = note
@@ -38,6 +39,10 @@ class note:
         self.delta_start = delta_start
     def cal_delta(self, delta_sum):
         self.now_delta = delta_sum - self.delta_start
+    def cal_last(self):
+        if self.length - self.now_delta >= 0: 
+            self.last_len = self.length - self.now_delta
+
 
 class rest_note(note):
     def __init__(self, length):
@@ -71,7 +76,7 @@ def cal_beat_to_tick(beat, ticks):
     return cal
 
 
-def off_maker(complist, count, before_delta, flag):
+def off_maker(complist, count, flag):
     global delta_sum
     for cal_i in reversed(complist):
         if flag == True:
@@ -80,17 +85,16 @@ def off_maker(complist, count, before_delta, flag):
         for s, cal_j in enumerate(cal_i): 
             if cal_j.note != 0:
                 cal_j.cal_delta(delta_sum)
+                cal_j.cal_last()
                 if cal_j.now_delta == cal_j.length:
-                    print('{} : '.format(cal_j.note))
                     track_list[count].append(Message('note_off', note=cal_j.note, velocity=64, time=0))
                     cal_i[s].note = 0 
                 if cal_j.now_delta > cal_j.length:
-                    cal_j.cal_delta(before_delta)
-                    track_list[count].append(Message('note_off', note=cal_j.note, velocity=64, time=cal_j.length - cal_j.now_delta))
-                    print('{} :  {} - {}'.format(cal_j.note,cal_j.length,  cal_j.now_delta))
-                    if cal_j.length - cal_j.now_delta < 0:
+                    track_list[count].append(Message('note_off', note=cal_j.note, velocity=64, time=cal_j.last_len))
+                    if cal_j.last_len < 0:
                         print('minus')
-                    delta_sum += cal_j.length - cal_j.now_delta
+                        time.sleep(3)
+                    delta_sum += cal_j.last_len
                     cal_i[s].note = 0 
 
 
@@ -98,7 +102,7 @@ def off_maker(complist, count, before_delta, flag):
 
 ################# << main start >> #################
 
-mid_analysis = MidiFile(r'D:\code\py\MMMake\midi\alstroemeria.mid')
+mid_analysis = MidiFile(r'D:\code\py\MMMake\midi\Taiyou+Iwaku+Moeyo+Chaos.mid')
 
 ticks = mid_analysis.ticks_per_beat
 
@@ -108,22 +112,19 @@ for i, track in enumerate(mid_analysis.tracks):
     delta = 0
     for msg in track:
         #delta cal
-        if msg.type == 'control_change' or msg.type == 'program_change':
-            delta += msg.time
-        if msg.is_meta:
+        if msg.type != 'note_on' and msg.type != 'note_off':
+            if msg_list:
+                if msg_list[-1].type == 'note_off':
+                    delta = 0
             delta += msg.time
             continue
         #타악기 제외
         if msg.channel == 9:
             break
-        if msg.type == 'note_on' or msg.type == 'note_off':
-            msg_list.append(msg)
+        msg_list.append(msg)
         if msg.type == 'note_on':
             ob_note = note(msg.note,msg.time+delta)
             if ob_note.ontime != 0:
-                if onlist:
-                    list.append(onlist)
-                    onlist = []
                 #쉼표넣기
                 ob_rest = rest_note(msg.time+delta)
                 onlist.append(ob_rest)
@@ -132,6 +133,9 @@ for i, track in enumerate(mid_analysis.tracks):
             onlist.append(ob_note)
             delta = 0
         if msg.type == 'note_off':
+            if msg_list:
+                if msg_list[-2].type == 'note_off':
+                    delta = 0
             cal_time = 0
             #onlist가 비어있는 경우는 note_off 메시지가 2번 연속 나온경우
             if onlist:
@@ -139,12 +143,14 @@ for i, track in enumerate(mid_analysis.tracks):
                 list.append(onlist)
             
             #노트 길이 계산
+            cal_time += delta
             for idx, k in enumerate(reversed(msg_list)):
                 if k.type == 'note_on' and msg_list[-1].note == k.note:
                     #한번 참조한 정보는 다시 참조안하게 note=0
                     msg_list[-(idx+1)] = k.copy(note=0)
                     break
-                cal_time += k.time + delta
+                cal_time += k.time
+
                 
             #노트 길이 입력, 입력이 안되어 있는 것만    
             for k in reversed(list):
@@ -157,7 +163,6 @@ for i, track in enumerate(mid_analysis.tracks):
                             off_flag = True
                             break
             off_flag = False
-            delta = 0
             onlist = []
     #노트 메시지가 없는 트랙은 삽입하지 않는다
     msg_list = []
@@ -166,14 +171,14 @@ for i, track in enumerate(mid_analysis.tracks):
         list = []
 
              
-for i in ListOfList:
+for count, i in enumerate(ListOfList):
+    print('Track {}'.format(count))
     for j in i:
         for k in j:
             print(k.note_string, end=' ')
             print()
         print()
     print()
-
 
 #파싱 미디파일 출력
 mid_create = MidiFile(ticks_per_beat = ticks)
@@ -187,23 +192,39 @@ track_list[0].append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(bpm), time=0)
 
 for count, i in enumerate(ListOfList):
     delta_sum = 0
-    before_delta = 0
     if count != 0:
         mid_create.tracks.append(track_list[count])
     for i_idx, j in enumerate(i):
         for idx, k in enumerate(j):
             complist = i[:i_idx+1]
-            before_delta = delta_sum
+            #쉼표노트 1개만 있는 것 처리
+            if len(j) == 1:
+                if type(k) is rest_note:
+                    ontime += k.length
+                    delta_sum += ontime
+                    off_maker(complist,count,False)
+                    break  
             if idx == 0:
                 shortest = k.length
                 minid = 0
             if k.length <= shortest:
                 shortest = k.length
                 minid = idx
+            #무조건 동시노트의 마지막 쉼표 때 실행됨
             if type(k) is rest_note:
                 ontime += k.length
-                delta_sum += ontime
-                off_maker(complist,count,before_delta,False)
+
+                delta_sum += shortest
+                off_maker(complist,count,True)
+
+                for msg in msg_list:
+                    track_list[count].append(msg)
+                msg_list = []
+                if j[minid].note != 0:
+                    track_list[count].append(Message('note_off', note=j[minid].note, velocity=64, time=shortest))
+                    j[minid].note = 0
+                off_maker(complist,count,False)
+
             else:
                 msg_list.append(Message('note_on', note=k.note, velocity=100, time=ontime))
                 k.set_delta(delta_sum)
@@ -211,14 +232,14 @@ for count, i in enumerate(ListOfList):
                 if k == j[-1]:
                     delta_sum += shortest
 
-                    off_maker(complist,count,before_delta,True)
+                    off_maker(complist,count,True)
 
                     for msg in msg_list:
                         track_list[count].append(msg)
                     msg_list = []
                     track_list[count].append(Message('note_off', note=j[minid].note, velocity=64, time=shortest))
                     j[minid].note = 0
-                    off_maker(complist,count,before_delta,False)
+                    off_maker(complist,count,False)
 
                     
 
